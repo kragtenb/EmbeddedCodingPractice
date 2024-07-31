5fa2ae26aeac887f289fc4df454910f73a309e3a
Blake Kragten <kragtenb@gmail.com>
2024-07-31 11:13:42 -0700

    Binary Search Tree Smart Pointers Implementation

--- InterviewPrep/C++/C++ Language Fundamentals/binary_search_tree_impl.cpp
+++ InterviewPrep/C++/C++ Language Fundamentals/binary_search_tree_impl.cpp
@@ -1,33 +1,69 @@
 #include <iostream>
 #include <memory>
 
-class Node {
-public:
-    Node(int val, Node* left, Node* right) {
-        this->val = val;
-        this->left = left;
-        this->right = right;
-    }
-
+struct Node {
     int val;
-    Node* left;
-    Node* right;
+    std::unique_ptr<Node> left;
+    std::unique_ptr<Node> right;
+
+    Node(int val) : val(val), left(nullptr), right(nullptr) {}
 };
 
 class BinarySearchTree {
 public:
     BinarySearchTree(int val) {
-        head = std::make_unique<Node>(new Node(val, nullptr, nullptr));
+        head = std::make_unique<Node>(val);
     }
 
-    void insert(int) {
-        std::
+    void insert(int val) {
+        head = insertInOrder(std::move(head), val);
     }
 
     void print() {
-
+        printInOrder(head.get());
+        std::cout << std::endl;
     }
 
 private:
     std::unique_ptr<Node> head;
+
+    std::unique_ptr<Node> insertInOrder(std::unique_ptr<Node> node, int val) {
+        if (node == nullptr) {
+            return std::make_unique<Node>(val);
+        }
+
+        if (val < node->val) {
+            node->left = insertInOrder(std::move(node->left), val);
+        } else if (val > node->val) {
+            node->right = insertInOrder(std::move(node->right), val);
+        }
+
+        return node;
+    }
+
+    void printInOrder(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        printInOrder(node->left.get());
+        std::cout << node->val << " ";
+        printInOrder(node->right.get());
+    }
 };
+
+int main() {
+    BinarySearchTree bst(20);
+
+    bst.insert(5);
+    bst.insert(4);
+    bst.insert(25);
+    bst.insert(40);
+    bst.insert(8);
+    bst.insert(7);
+
+    bst.print();
+    bst.print();
+    
+    return 0;
+}
